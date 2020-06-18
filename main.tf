c16772d923d72eee4c895405e3fe8912fed64428
Farhan Akthar <tech_fur@outlook.com>
2020-06-18 10:54:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
     region      = "${var.region}"
-    shared_credentials_file = "$HOME/.aws/credentials"
-    profile                 = "default"
+    access_key = "AKIA3T3H7KH5ET6HTT55"
+ secret_key = "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"
+  profile = "dev"
     
 }
 data "aws_availability_zones" "available" {}
